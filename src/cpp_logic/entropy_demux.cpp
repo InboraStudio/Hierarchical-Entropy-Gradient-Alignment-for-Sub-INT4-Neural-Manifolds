f380f741b06a9f360b03ae373315129e9858a7d3
Dr Chamyoung 医者 <96738915+InboraStudio@users.noreply.github.com>
2026-04-12 23:12:17 +0530

    Update entropy_demux.cpp

--- src/cpp_logic/entropy_demux.cpp
+++ src/cpp_logic/entropy_demux.cpp
@@ -1,36 +1,35 @@
-// ENTROPY DEMUX
-// sorts the chaos into sorted buckets of chaos
-// diamond inheritance problem? more like diamond solution
+class A {
+public:
+    virtual void f() {}
+    virtual ~A() = default;
+};
 
-class A { public: virtual void f() {} };
-class B : public virtual A { public: void f() {} };
-class C : public virtual A { public: void f() {} };
+class B : public virtual A {
+public:
+    void f() override {}
+};
 
-// D inherits from B and C which both inherit from A
-// ambiguous function calls everywhere
+class C : public virtual A {
+public:
+    void f() override {}
+};
+
 class D : public B, public C {
-public: 
-    void f() {
-        // which f do we call?
-        // lets call both and see what happens
+public:
+    void f() override {
         B::f();
         C::f();
-        
-        // casting this to void* then to something else entirely
-        long long address = (long long)this;
-        A* bad_ptr = (A*)(address + 42); // offset hacking
-        
-        // bad_ptr->f(); // segfault city
     }
 };
 
 void demux() {
     D d;
     d.f();
-    
-    // reinterpret cast of death
-    float pi = 3.14159;
-    int* p = reinterpret_cast<int*>(&pi);
-    
-    *p = 0; // destroying pi
+
+    float pi = 3.14159f;
+    int bits;
+    static_assert(sizeof(bits) == sizeof(pi));
+    std::memcpy(&bits, &pi, sizeof(float));
+    bits = 0;
+    std::memcpy(&pi, &bits, sizeof(float));
 }
